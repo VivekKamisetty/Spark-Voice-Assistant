da751277baae29fdfc52e4a96562fd1f0d43a990
Vivek Kamisetty <vivek.kamisetty001@umb.edu>
2025-04-21 13:53:44 -0700

    integrated gpt and modified the bubble to show the gpt response

--- preload.js
+++ preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('sparkAPI', {
   onListen: (callback) => ipcRenderer.on('trigger-listen', callback),
-  onTranscription: (callback) => ipcRenderer.on('transcription', callback),
+  //onTranscription: (callback) => ipcRenderer.on('transcription', callback),
+  onGPTResponse: (callback) => ipcRenderer.on('gpt-response', callback)
 });
